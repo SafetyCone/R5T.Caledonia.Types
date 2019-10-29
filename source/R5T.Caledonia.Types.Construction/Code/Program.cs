d420241a62906ffbc76d69593eb7cec092d0c6e6
MinexAutomation <minex.automation@gmail.com>
2019-10-29 13:04:49 -0700

    Initial commit of source-stub.

---
+++ source/R5T.Caledonia.Types.Construction/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.Caledonia.Types.Construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
