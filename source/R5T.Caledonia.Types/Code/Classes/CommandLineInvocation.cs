2448f52896c3f53c2e1b7046b64be8667e3d9b42
MinexAutomation <minex.automation@gmail.com>
2019-10-29 22:30:58 -0700

    Added static New() construction method, and default data handlers.

--- source/R5T.Caledonia.Types/Code/Classes/CommandLineInvocation.cs
+++ source/R5T.Caledonia.Types/Code/Classes/CommandLineInvocation.cs
@@ -6,6 +6,44 @@ namespace R5T.Caledonia
 {
     public class CommandLineInvocation
     {
+        #region Static
+
+        public static CommandLineInvocation New(string command, string arguments)
+        {
+            var invocation = new CommandLineInvocation()
+            {
+                Command = command,
+                Arguments = arguments,
+                ReceiveOutputData = CommandLineInvocation.DefaultReceiveOutputData,
+                ReceiveErrorData = CommandLineInvocation.DefaultReceiveErrorData,
+            };
+
+            return invocation;
+        }
+
+        public static void DefaultReceiveOutputData(object sender, DataReceivedEventArgs dataReceived)
+        {
+            if(dataReceived.Data is null)
+            {
+                return;
+            }
+
+            Console.WriteLine(dataReceived.Data);
+        }
+
+        public static void DefaultReceiveErrorData(object sender, DataReceivedEventArgs dataReceived)
+        {
+            if (dataReceived.Data is null)
+            {
+                return;
+            }
+
+            Console.WriteLine(dataReceived.Data);
+        }
+
+        #endregion
+
+
         public string Command { get; set; }
         public string Arguments { get; set; }
         public DataReceivedEventHandler ReceiveOutputData { get; set; }
